731d6efd9cfe0839ac852a147b9a129476ca7ca6
arsrivastawa <arsrivastawa2710@gmail.com>
2026-09-03 22:33:17 +0530

    feat: wired up dependencies and started HTTP server in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,19 +5,33 @@ import (
 	"net/http"
 
 	"github.com/arsrivastawa/shawttyy/internal/api"
+	"github.com/arsrivastawa/shawttyy/internal/core/sequencer"
+	"github.com/arsrivastawa/shawttyy/internal/service"
+	"github.com/arsrivastawa/shawttyy/internal/storage"
+)
+
+const (
+	baseURL       = "http://localhost:8080"
+	defaultNodeID = 1
 )
 
 func main() {
 	fmt.Println("The Shawttyy is up and running!!!")
 
-	mux := http.NewServeMux()
-
-	api.RegisterHelloRouter(mux)
-
-	err := http.ListenAndServe(":8080", mux)
-
+	seq, err := sequencer.New(defaultNodeID)
 	if err != nil {
-		fmt.Printf("Error starting server: %v\n", err)
+		panic(err)
 	}
 
+	store := storage.NewInMemoryStore()
+	svc := service.New(store, seq)
+	handler := api.NewHandler(svc, baseURL)
+
+	mux := http.NewServeMux()
+	api.RegisterRoutes(mux, handler)
+
+	fmt.Printf("Listening on %s\n", baseURL)
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 }
